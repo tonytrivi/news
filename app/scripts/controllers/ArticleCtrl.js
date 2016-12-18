21eaf0a826cf02f870387b5f61a0d640bc70670a
Tony Trivison <shambles72@hotmail.com>
2016-12-18 09:39:13 -0800

    styles on collection items

--- app/scripts/controllers/ArticleCtrl.js
+++ app/scripts/controllers/ArticleCtrl.js
@@ -2,22 +2,25 @@
     function ArticleCtrl() {
       this.desc = "Article Page";
       this.articles = {
-        date: new Date(2016, 12, 07),
-        articleList: [{
-          title: "The Joy of Legislating",
-          url: "http://www.microsoft.com",
-          summary: "Congress got together",
-          photo: "",
-          writer: "Joey Reporter",
-          publication: "L.A. Times"
-          }, {
-          title: "Big Time on Capitol Hill",
-          url: "http://www.apple.com",
-          summary: "We had an interview",
-          photo: "",
-          writer: "Sally Writer",
-          publication: "O.C. Register"
-          }]
+        description: "article collection",
+        batch: [{
+          date: new Date(2016, 12, 07),
+          articleList: [{
+            title: "The Joy of Legislating",
+            url: "http://www.microsoft.com",
+            summary: "Congress got together",
+            photo: "",
+            writer: "Joey Reporter",
+            publication: "L.A. Times"
+            }, {
+            title: "Big Time on Capitol Hill",
+            url: "http://www.apple.com",
+            summary: "We had an interview",
+            photo: "",
+            writer: "Sally Writer",
+            publication: "O.C. Register"
+            }]
+        }]
       };
     }
 
