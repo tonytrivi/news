377c965c0f900bbc1672d2f0810063e63916ad9c
Tony Trivison <shambles72@hotmail.com>
2016-12-18 08:28:21 -0800

    adding article collection

--- app/scripts/controllers/ArticleCtrl.js
+++ app/scripts/controllers/ArticleCtrl.js
@@ -1,6 +1,24 @@
 (function() {
     function ArticleCtrl() {
-      this.name = "Louis";
+      this.desc = "Article Page";
+      this.articles = {
+        date: new Date(2016, 12, 07),
+        articleList: [{
+          title: "The Joy of Legislating",
+          url: "http://www.microsoft.com",
+          summary: "Congress got together",
+          photo: "",
+          writer: "Joey Reporter",
+          publication: "L.A. Times"
+          }, {
+          title: "Big Time on Capitol Hill",
+          url: "http://www.apple.com",
+          summary: "We had an interview",
+          photo: "",
+          writer: "Sally Writer",
+          publication: "O.C. Register"
+          }]
+      };
     }
 
     angular
