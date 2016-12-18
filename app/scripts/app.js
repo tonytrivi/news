e6e9d79ed332dff807d4038d1ffc3d313441fc33
Tony Trivison <shambles72@hotmail.com>
2016-12-17 17:32:02 -0800

    adding routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,22 @@
-angular.module('news', []);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+          .html5Mode({
+          enabled: true,
+          requireBase: false
+        });
+
+        $stateProvider
+          .state('article', {
+            url: '/',
+            templateUrl: '/templates/article.html'
+          })
+          .state('summary', {
+              url: '/summary',
+              templateUrl: '/templates/summary.html'
+          });
+    }
+    angular
+        .module('news', ['ui.router'])
+        .config(config);
+})();
