a47fe34bd1093288001db43894a2e2c50790e131
Tony Trivison <shambles72@hotmail.com>
2016-12-30 13:52:55 -0800

    test suite does not work

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
 describe('App Tests', function() {
-  describe('Testing angularjs controllers', function (){
+  describe('Testing article controller', function (){
     var scope;
+    var firebaseApp;
 
     beforeEach(angular.mock.module('news','firebase'));
     beforeEach(angular.mock.inject(function($controller,$rootScope,$firebase){
         scope = $rootScope.$new();
+
         var config = {
           apiKey: "AIzaSyDOwaiOKq9q-SH-VJ6nK0ZFVTT2kxnPlxM",
           authDomain: "statecraft-dcbc6.firebaseapp.com",
@@ -12,17 +14,24 @@ describe('App Tests', function() {
           storageBucket: "statecraft-dcbc6.appspot.com",
           messagingSenderId: "5208094240"
         };
-        firebase.initializeApp(config);
+        firebaseApp = firebase.initializeApp(config);
+
+        //console.log(firebaseApp);
 
         $controller('ArticleCtrl as article', {$scope: scope});
     }));
-    
-    it('should be 2', function(){
-        expect(scope.article).toBeDefined();
-        expect(scope.article.articleCollectionLimit).toBe(2);
 
+    it('should be defined', function(){
+      expect(scope.article).toBeDefined();
     });
 
+    it('should be 2', function(){
+      expect(scope.article.articleCollectionLimit).toBe(2);
+    });
+
+    afterEach(function() {
+            firebaseApp.delete();
+        });
   });
 
 });
