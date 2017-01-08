24086e746b22b2abb97cb00b6b478e4ab720ab9d
Tony Trivison <shambles72@hotmail.com>
2017-01-07 22:17:28 -0800

    roughing out login logic

--- app/scripts/controllers/LoginCtrl.js
+++ app/scripts/controllers/LoginCtrl.js
@@ -11,11 +11,39 @@
       this.logIn = function() {
         console.log(this.email);
         console.log(this.password);
+
+        var authObj = firebase.auth();
+        console.log(authObj);
+
+        //sign up
+        //authObj.createUserWithEmailAndPassword(this.email, this.password).catch(function(error) {
+        //  var errorCode = error.code;
+        //  var errorMessage = error.message;
+        //});
+
+        authObj.signInWithEmailAndPassword(this.email, this.password).catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+        });
+
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+           console.log(user);
+           //here you can show elements that the logged in user should see
+           //show a log out button
+          } else {
+            console.log('not logged in');
+            //show the login controls
+          }
+        });
+
+        this.email = '';
+        this.password = '';
       };
 
     }
 
     angular
         .module('news')
-        .controller('LoginCtrl', LoginCtrl);
+        .controller('LoginCtrl', [LoginCtrl]);
 })();
